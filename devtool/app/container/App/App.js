b3f3754d557d127f1ddf8a58ec95236c1118fdc9
Kiikurage <y.kikura@gmail.com>
2015-12-17 23:17:39 +0900

    rename

--- devtool/app/container/App/App.js
+++ devtool/app/container/App/App.js
@@ -6,9 +6,6 @@ import { ThemeManager } from 'material-ui/lib/styles'
 import React, { Component, PropTypes } from 'react'
 import AppBar from 'material-ui/lib/app-bar'
 
-import Pager from 'components/Pager/Pager'
-import Page from 'components/Page/Page'
-import Card from 'components/Card/Card'
 import SignUpForm from 'components/SignUpForm/SignUpForm'
 import SignInForm from 'components/SignInForm/SignInForm'
 import PlanViewer from 'components/PlanViewer/PlanViewer'
@@ -27,7 +24,7 @@ export default class App extends Component {
 	render() {
 		return (
             <div className="App">
-                <AppBar title="旅ログ"></AppBar>
+                <AppBar title="Izu developoment tool"></AppBar>
                 <div className="App__pagerWrapper">
                     <SignUpForm></SignUpForm>
                     <SignInForm></SignInForm>
